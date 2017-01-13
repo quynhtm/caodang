2483b1772b8c5134187caca366edfd16d840632a
nguyenduypt86@gmail.com <nguyenduypt86@gmail.com>
2017-01-13 22:36:13 +0700

    duy

---
+++ app/views/site/BaseLayouts/footer.blade.php
@@ -0,0 +1,8 @@
+<div class="container">
+	<div class="wraps-footer">
+		<div style="text-align: center;"><b>Cơ quan chủ quản:</b> <b>Trường Đại học Điện lực</b></div>
+		<div style="text-align: center;"><span class="Apple-style-span">Địa chỉ: 235 Hoàng Quốc Việt, Hà Nội</span></div>
+		<div style="text-align: center;"><span class="Apple-style-span">Điện thoại: Tuyển sinh: 04 2245 2662, Hành chính Quản trị: 04 2218 5607<br>Fax: 04 3836 2065</span></div>
+		<div style="text-align: center;">Copyright @2011 by EPU</div>
+	</div>
+</div>
--- app/views/site/SiteLayouts/Home.blade.php
+++ app/views/site/SiteLayouts/Home.blade.php
@@ -12,10 +12,140 @@
 								<a href="">Giới thiệu</a>
 							</span>
 						</div>
+						<div class="list">
+							<ul>
+							   <li><a href="" title="Thông tin khái quát">Thông tin khái quát</a></li>
+							   <li><a href="" title="Chức năng và nhiệm vụ">Chức năng và nhiệm vụ</a></li>
+							   <li><a href="" title="Ban giám hiệu">Ban giám hiệu</a></li>
+							   <li><a href="" title="Cơ cấu tổ chức">Cơ cấu tổ chức</a></li>
+							   <li><a href="" title="Cơ sở vật chất">Cơ sở vật chất</a></li>
+							   <li><a href="" title="Báo cáo Ba công khai">Báo cáo Ba công khai</a></li>
+							   <li><a href="" title="Chuẩn đầu ra">Chuẩn đầu ra</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="item-box">
+						<div class="top-box">
+							<span>
+								<a href="">Tin tức</a>
+							</span>
+						</div>
+						<div class="list">
+							<ul>
+							   <li><a href="" title="Thông tin khái quát">Đại học Điện lực</a></li>
+							   <li><a href="" title="Chức năng và nhiệm vụ">Ngành điện</a></li>
+							   <li><a href="" title="Ban giám hiệu">Khoa học-Công nghệ</a></li>
+							   <li><a href="" title="Cơ cấu tổ chức">HN NCKH Quốc tế</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="item-box">
+						<div class="top-box">
+							<span>
+								<a href="">Đào tạo</a>
+							</span>
+						</div>
+						<div class="list">
+							<ul>
+							   <li><a href="" title="Thông báo đào tạo">Thông báo đào tạo</a></li>
+							   <li><a href="" title="Thông báo điểm">Thông báo điểm</a></li>
+							   <li><a href="" title="Đào tạo Đại học-Cao đẳng">Đào tạo Đại học-Cao đẳng</a></li>
+							   <li><a href="" title="">Đào tạo Chất lượng cao</a></li>
+							   <li><a href="" title="Đào tạo TCCN">Đào tạo TCCN</a></li>
+							   <li><a href="" title="Đào tạo nghề">Đào tạo nghề</a></li>
+							   <li><a href="" title="Đào tạo liên thông">Đào tạo liên thông</a></li>
+							   <li><a href="" title="Đào tạo Sau Đại học">Đào tạo Sau Đại học</a></li>
+							   <li><a href="" title="">Liên kết đào tạo Quốc tế</a></li>
+							   <li><a href="" title="Quy chế đào tạo">Quy chế đào tạo</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="item-box">
+						<div class="top-box">
+							<span>
+								<a href="">Tuyển sinh</a>
+							</span>
+						</div>
+						<div class="list">
+							<ul>
+							   <li><a href="" title="Cổng Thông tin Tuyển sinh">Cổng Thông tin Tuyển sinh</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="item-box">
+						<div class="top-box">
+							<span>
+								<a href="">Nghiên cứu khoa học</a>
+							</span>
+						</div>
+						<div class="list">
+							<ul>
+							   <li><a href="" title="Nghiên cứu khoa học">Nghiên cứu khoa học</a></li>
+							   <li><a href="" title="Hợp tác Quốc tế">Hợp tác Quốc tế</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="item-box">
+						<img alt="" src="{{URL::route('site.home')}}/assets/frontend/img/IEEElogo.jpg">
+					</div>
+					<div class="item-box">
+						<img alt="" src="{{URL::route('site.home')}}/assets/frontend/img/Libol.jpg">
 					</div>
 				</div>
+				<div class="col-middle">
+				
+				</div>
 				<div class="col-right">
-					
+					<div class="item-box">
+						<div class="top-box">
+							<span>
+								<a href="">Sinh viên</a>
+							</span>
+						</div>
+						<div class="list">
+							<ul>
+							   <li><a href="" title="Nghiên cứu khoa học">Hoạt động đoàn thể</a></li>
+							   <li><a href="" title="Hợp tác Quốc tế">Góc học tập</a></li>
+							    <li><a href="" title="Nghiên cứu khoa học">Kinh nghiệm việc làm</a></li>
+							   <li><a href="" title="Hợp tác Quốc tế">Cơ hội việc làm</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="item-box">
+						<div class="top-box">
+							<span>
+								<a href="">Cựu sinh viên</a>
+							</span>
+						</div>
+						<div class="list">
+							<ul>
+							   <li><a href="" title="Nghiên cứu khoa học">Hoạt động</a></li>
+							   <li><a href="" title="Hợp tác Quốc tế">Phản hồi việc làm</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="item-box">
+						<div class="top-box">
+							<span>
+								<a href="">Thư giãn</a>
+							</span>
+						</div>
+						<div class="list">
+							<ul>
+							   <li><a href="" title="Nghiên cứu khoa học">Âm nhạc</a></li>
+							   <li><a href="" title="Hợp tác Quốc tế">Cười</a></li>
+							    <li><a href="" title="Nghiên cứu khoa học">Góc thơ văn</a></li>
+							   <li><a href="" title="Hợp tác Quốc tế">Văn hoá</a></li>
+							   <li><a href="" title="Hợp tác Quốc tế">Ảnh ngộ nghĩnh</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="item-box">
+						<img alt="" src="{{URL::route('site.home')}}/assets/frontend/img/hoc phi copy.jpg">
+					</div>
+					<div class="item-box">
+						<img alt="" src="{{URL::route('site.home')}}/assets/frontend/img/dkmh1.png">
+					</div>
 				</div>
 			</div>
 		</div>
