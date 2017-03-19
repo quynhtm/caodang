aad9bf0d4380546bea0e5631d4a56f0330e22b03
nguyenduypt86@gmail.com <nguyenduypt86@gmail.com>
2017-03-19 13:50:17 +0700

    duy

--- app/views/site/BaseLayouts/header.blade.php
+++ app/views/site/BaseLayouts/header.blade.php
@@ -17,32 +17,47 @@
 				<a class="en class=" href="?lang=en"></a>
 			</div>
 	    </div>
-		<div class="bg-menu">
+		<nav class="navbar navbar-inverse">
 			<div class="container">
-				<ul class="menu">
-					<li><a class="aline" href="{{URL::route('site.home')}}">Trang chủ</a></li>
-					@if(!empty($menuCategoriessAll))
-                        <?php $i=1; ?>
-						@foreach($menuCategoriessAll as $cat)
-							@if($i <= $numCategory)
-								@if($cat['category_show_top'] == CGlobal::status_show && $cat['category_parent_id'] == CGlobal::status_hide)
-                                    <?php $i++; ?>
-									<li><a class="aline @if(isset($catid) && $catid == $cat['category_id']) act @endif" @if($cat['category_link'] != '')href="{{$cat['category_link']}}" target="_blank" @else href="{{FunctionLib::buildLinkCategory($cat['category_id'], $cat['category_name'])}}" @endif title="{{$cat['category_name']}}">{{$cat['category_name']}}</a>
-										<ul class="menu-sub">
-											@foreach($menuCategoriessAll as $sub)
-												@if($sub['category_parent_id'] == $cat['category_id'] && $sub['category_show_top'] == CGlobal::status_show)
-													<li><a @if($sub['category_link'] != '')href="{{$sub['category_link']}}" target="_blank" @else href="{{FunctionLib::buildLinkCategory($sub['category_id'], $sub['category_name'])}}" @endif title="{{$sub['category_name']}}">{{$sub['category_name']}}</a></a></li>
-												@endif
-											@endforeach
-										</ul>
-									</li>
-								@endif
-							@endif
-						@endforeach
-					@endif
-					<li><a href="{{URL::route('site.pageContact')}}" title="Liên hệ">Liên hệ</a></li>
-				</ul>
+				<div class="top-header-button">
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+								data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+							<span class="sr-only">Menu icon</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+					</div>
+				</div>
 			</div>
-		</div>
+			<div class="bg-menu">
+				<div id="navbar" class="collapse navbar-collapse">
+					<ul class="menu nav navbar-nav menu-header ">
+						<li><a class="aline" href="{{URL::route('site.home')}}">Trang chủ</a></li>
+						@if(!empty($menuCategoriessAll))
+                            <?php $i=1; ?>
+							@foreach($menuCategoriessAll as $cat)
+								@if($i <= $numCategory)
+									@if($cat['category_show_top'] == CGlobal::status_show && $cat['category_parent_id'] == CGlobal::status_hide)
+                                        <?php $i++; ?>
+										<li><a class="aline @if(isset($catid) && $catid == $cat['category_id']) act @endif" @if($cat['category_link'] != '')href="{{$cat['category_link']}}" target="_blank" @else href="{{FunctionLib::buildLinkCategory($cat['category_id'], $cat['category_name'])}}" @endif title="{{$cat['category_name']}}">{{$cat['category_name']}}</a>
+											<ul class="menu-sub">
+												@foreach($menuCategoriessAll as $sub)
+													@if($sub['category_parent_id'] == $cat['category_id'] && $sub['category_show_top'] == CGlobal::status_show)
+														<li><a @if($sub['category_link'] != '')href="{{$sub['category_link']}}" target="_blank" @else href="{{FunctionLib::buildLinkCategory($sub['category_id'], $sub['category_name'])}}" @endif title="{{$sub['category_name']}}">{{$sub['category_name']}}</a></a></li>
+													@endif
+												@endforeach
+											</ul>
+										</li>
+									@endif
+								@endif
+							@endforeach
+						@endif
+						<li><a href="{{URL::route('site.pageContact')}}" title="Liên hệ">Liên hệ</a></li>
+					</ul>
+				</div>
+			</div>
+		</nav>
 	</div>
 </div>
