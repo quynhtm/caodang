696bd51c6dbc66a55f8613b93bbe71b4776fb235
nguyenduypt86@gmail.com <nguyenduypt86@gmail.com>
2017-03-19 14:35:12 +0700

    duy

---
+++ app/views/site/SiteLayouts/pageDepartment.blade.php
@@ -0,0 +1,6 @@
+<div class="col-lg-6 col-md-6 col-sm-12 contact col-middle">
+	<h1 class="title-path"><a title="{{$item['department_name']}}" href="{{FunctionLib::buildLinkCategoryDepartment($item['department_alias'], $item['department_name'], $item['department_id'])}}">{{$item->department_name}}</a></h1>
+	<div class="list-library">
+		
+	</div>
+</div>
