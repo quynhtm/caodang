9c3062371ddc2a07e1227ba23a430c9f698ac081
nguyenduypt86@gmail.com <nguyenduypt86@gmail.com>
2017-03-29 18:13:58 +0700

    duy

--- app/controllers/site/TraCuuController.php
+++ app/controllers/site/TraCuuController.php
@@ -4,8 +4,30 @@ class TraCuuController extends BaseSiteController{
         parent::__construct();
     }
 	public function traCuuVanBangChungChi(){
-		echo 'traCuuVanBangChungChi';die;
+        //Meta title
+        $meta_title='';
+        $meta_keywords='';
+        $meta_description='';
+        $meta_img='';
+        $arrMeta = Info::getItemByKeyword('SITE_SEO_TRACUU_VANBANG_CHUNGCHI');
+        if(!empty($arrMeta)){
+            $meta_title = $arrMeta->meta_title;
+            $meta_keywords = $arrMeta->meta_keywords;
+            $meta_description = $arrMeta->meta_description;
+            $meta_img = $arrMeta->info_img;
+            if($meta_img != ''){
+                $meta_img = ThumbImg::thumbBaseNormal(CGlobal::FOLDER_INFO, $arrMeta->info_id, $arrMeta->info_img, 550, 0, '', true, true);
+            }
+        }
+        FunctionLib::SEO($meta_img, $meta_title, $meta_keywords, $meta_description);
+
+        $this->header();
+        $this->layout->content = View::make('site.SiteLayouts.pageTraCuuVanBangChungChi');
+        $this->eduBottom();
+        $this->sliderPartnerBottom();
+        $this->footer();
 	}
+	
 	public function traCuuDiemThiNangKhieu(){
 		echo 'traCuuDiemThiNangKhieu';die;
 	}
