ec16e66b9a4640be7b6e05985cd8f9b71da5f899
nguyenduypt86@gmail.com <nguyenduypt86@gmail.com>
2017-04-17 15:20:26 +0700

    duy

--- app/views/site/BaseLayouts/index.blade.php
+++ app/views/site/BaseLayouts/index.blade.php
@@ -4,6 +4,7 @@
 	{{CGlobal::$extraMeta}}
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
 	<link rel="shortcut icon" href="{{Config::get('config.WEB_ROOT')}}assets/frontend/img/favicon.ico" type="image/vnd.microsoft.icon">
 		
 	{{ HTML::style('assets/lib/bootstrap/css/bootstrap.css'); }}
